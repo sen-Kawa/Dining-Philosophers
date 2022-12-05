f16bfd22f3f42923b5b7761755cab3109a776708
olin_tp_ar <universalexperimental@protonmail.com>
2022-12-05 14:49:32 +0100

    time function

--- philo/time.c
+++ philo/time.c
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:42:41 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/12/05 14:43:16 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/12/05 14:49:27 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 int64_t	time_stamp(void)
 {
+	struct timeval tv;
 	
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
