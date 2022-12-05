eb764f303f3b3b7079f62ca4653a46abb0de9c70
olin_tp_ar <universalexperimental@protonmail.com>
2022-12-05 23:40:01 +0100

    forgot to add routines file

---
+++ philo/routines.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routines.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 14:32:03 by kaheinz           #+#    #+#             */
+/*   Updated: 2022/12/05 23:39:28 by kaheinz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*routine(void *data)
+{
+	t_philo	*philo;
+	t_args	*args;
+
+	philo = (t_philo *)data;
+	args = philo->args;
+	if (philo->philo_id % 2 == 0)
+		usleep(args->time_eat * 1000);
+	pthread_mutex_lock(&args->meal_mutex);
+	philo->previous_meal = time_stamp();
+	pthread_mutex_unlock(&args->meal_mutex);
+	while (1)
+	{
+		actions(philo, args);
+	}
+	return (0);
+}
+
+void	actions(t_philo *philo, t_args *args)
+{
+	//eating
+	//sleeping
+	//thinking	
+}
+
+void	eating()
+{
+	//grab fork
+	//grab the other damn fork
+	//eat for time to eat
+	//mutex for time stamp for previous meal
+	//add to times eaten
+	//unlock forks
+}	
+
+void	sleeping()
+{
+
+}
+
+void	thinking()
+{
+
+}
