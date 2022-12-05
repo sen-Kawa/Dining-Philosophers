ea377adfcb429f7b139415b34ee1bf335a179000
olin_tp_ar <universalexperimental@protonmail.com>
2022-12-06 00:16:22 +0100

    sleeping for the required time

--- philo/routines.c
+++ philo/routines.c
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:32:03 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/12/06 00:07:50 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/12/06 00:16:07 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,18 +40,16 @@ void	actions(t_philo *philo, t_args *args)
 
 void	eating(t_philo *philo, t_args *args)
 {
-	(void)args;
 //	pthread_mutex_lock(&(args->fork_mutex[philo->l_fork]));
 	print_message(philo, "has taken a fork.");
 //	pthread_mutex_lock(&(args->fork_mutex[philo->r_fork]));
 	print_message(philo, "has taken a fork.");
-	//eat for time to
 	pthread_mutex_lock(&(args->meal_mutex));
 	philo->previous_meal = time_stamp();
-	//mutex for time stamp for previous meal
 	pthread_mutex_unlock(&(args->meal_mutex));
 	usleep(args->time_eat * 1000);
 	//add to times eaten
+	philo->times_eaten += 1;
 	//unlock forks
 //	pthread_mutex_unlock(&(args->fork_mutex[philo->l_fork]));
 //	pthread_mutex_unlock(&(args->fork_mutex[philo->r_fork]));
@@ -59,7 +57,7 @@ void	eating(t_philo *philo, t_args *args)
 
 void	sleeping(t_philo *philo, t_args *args)
 {
-	(void)args;
+	usleep(args->time_sleep * 1000);
 	print_message(philo, "is sleeping.");
 }
 
