fcf821ba453b1726d72c9bc6a745f3918516b91a
olin_tp_ar <universalexperimental@protonmail.com>
2022-12-05 23:55:52 +0100

    printing messages for each action, without any waiting time

--- philo/routines.c
+++ philo/routines.c
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 14:32:03 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/12/05 23:52:16 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/12/05 23:55:27 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,21 +42,22 @@ void	eating(t_philo *philo, t_args *args)
 {
 	pthread_mutex_lock(&(args->fork_mutex[philo->l_fork]));
 	print_message(philo, "has taken a fork.");
-	//grab fork
 	pthread_mutex_lock(&(args->fork_mutex[philo->r_fork]));
-	//grab the other damn fork
+	print_message(philo, "has taken a fork.");
 	//eat for time to eat
 	//mutex for time stamp for previous meal
 	//add to times eaten
 	//unlock forks
+	pthread_mutex_unlock(&(args->fork_mutex[philo->l_fork]));
+	pthread_mutex_unlock(&(args->fork_mutex[philo->r_fork]));
 }	
 
 void	sleeping(t_philo *philo, t_args *args)
 {
-
+	print_message(philo, "is sleeping.");
 }
 
 void	thinking(t_philo *philo, t_args *args)
 {
-
+	print_message(philo, "is thinking.");
 }
